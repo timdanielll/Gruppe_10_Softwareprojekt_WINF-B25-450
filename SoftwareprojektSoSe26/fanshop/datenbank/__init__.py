49c8dfb27c4c4f743e518b405fcf56964ed6d930
timdanielll <tida00005@htwsaar.de>
2026-08-21 16:31:31 +0200

    Fehler in der gewünschten Ordnerstruktur bereinigt.

--- SoftwareprojektSoSe26/fanshop/datenbank/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Datenbankschicht: Verbindung, Tabellenschema und Testdaten."""
-
-from fanshop.datenbank.verbindung import Datenbank, datenbank_vorbereiten
-
-__all__ = ["Datenbank", "datenbank_vorbereiten"]
