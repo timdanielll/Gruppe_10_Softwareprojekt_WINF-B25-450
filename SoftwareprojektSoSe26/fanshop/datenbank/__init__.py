7508c0a85408a8eb6583e16c24dcfd2708d7c886
Michail <miil00002@htwsaar.de>
2026-08-21 16:14:04 +0200

    feat:Datenbankschema mit Stamm-und Bewegungsdaten

---
+++ SoftwareprojektSoSe26/fanshop/datenbank/__init__.py
@@ -0,0 +1,5 @@
+"""Datenbankschicht: Verbindung, Tabellenschema und Testdaten."""
+
+from fanshop.datenbank.verbindung import Datenbank, datenbank_vorbereiten
+
+__all__ = ["Datenbank", "datenbank_vorbereiten"]
